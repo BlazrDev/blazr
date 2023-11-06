ab95844bc6c6bf9adcf392280d7d514cf4906644
Chameleeon <danichelsea3@gmail.com>
2023-11-06 16:14:44 +0100

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,116 @@
+workspace "Blazr"
+architecture "x64"
+
+configurations
+{
+  "Debug",
+  "Release",
+  "Dist"
+}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{architecture}"
+
+project "Blazr"
+location "Blazr"
+kind "SharedLib"
+language "C++"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("obj/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+  "%{prj.name}/src/**.h",
+  "%{prj.name}/src/**.cpp"
+}
+
+includedirs
+{
+  "%{prj.name}/vendor/spdlog/include",
+  "Blazr/src"
+}
+
+filter "system:windows"
+cppdialect "C++17"
+staticruntime "On"
+systemversion "latest"
+
+defines
+{
+  "BLZR_PLATFORM_WINDOWS",
+  "BLZR_BUILD_DLL",
+}
+
+postbuildcommands
+{
+  ("{COPY} %{cfg.buildtarget.relpatht} ../bin/" .. outputdir .. "/Sandbox")
+}
+
+filter "configurations:Debug"
+defines "HZ_DEBUG"
+symbols "On"
+
+filter "configurations:Release"
+defines "BLZR_RELEASE"
+optimize "On"
+
+filter "configurations:Dist"
+defines "BLZR_DIST"
+optimize "On"
+
+filter { "system:windows", "configurations:Release" }
+buildoptions "/MT"
+
+project "Sandbox"
+location "Sandbox"
+kind "ConsoleApp"
+language "C++"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("obj/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+  "%{prj.name}/src/**.h",
+  "%{prj.name}/src/**.cpp"
+}
+
+includedirs
+{
+  "Blazr/vendor/spdlog/include",
+  "Blazr/src"
+}
+
+links {
+  "Blazr"
+}
+
+filter "system:windows"
+cppdialect "C++17"
+staticruntime "On"
+systemversion "latest"
+
+defines
+{
+  "BLZR_PLATFORM_WINDOWS",
+}
+
+postbuildcommands
+{
+  ("{COPY} %{cfg.buildtarget.relpatht} ../bin/" .. outputdir .. "/Sandbox")
+}
+
+filter "configurations:Debug"
+defines "HZ_DEBUG"
+symbols "On"
+
+filter "configurations:Release"
+defines "BLZR_RELEASE"
+optimize "On"
+
+filter "configurations:Dist"
+defines "BLZR_DIST"
+optimize "On"
+
+filter { "system:windows", "configurations:Release" }
+buildoptions "/MT"
