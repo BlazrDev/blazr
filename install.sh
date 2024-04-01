27bdbad14a5936f568e52c3381f220be9e2ceb03
Chameleeon <danichelsea3@gmail.com>
2024-04-01 11:25:41 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+file_path=".gitmodules"
+if[ ! -e "$file_path"]; then
+  git submodule init
+fi
+
+dir_path = "bin/Debug-linux-x86_64/Sandbox"
+if[ ! -e "$dir_path"]; then
+  mkdir -p "$dir_path"
+  if[ $? -eq 0]; then
+    echo "Could not create directory: /bin/Debug-linux-x86_64/Sandbox"
+  fi
+fi
 echo "Setting up Blazr..."
-git submodule init
 git submodule update
 premake5 gmake
 premake5 vs2022
