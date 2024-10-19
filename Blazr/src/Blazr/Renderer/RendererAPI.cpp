9316c1df9b328d3e56143c740708aaff9288561c
Srkless <SrdjanGrujic01@outlook.com>
2024-10-19 18:06:02 +0200

    Picture render

--- Blazr/src/Blazr/Renderer/RendererAPI.cpp
+++ Blazr/src/Blazr/Renderer/RendererAPI.cpp
@@ -15,15 +15,8 @@ bool RendererAPI::Init() {
 
 	// TODO remove shader test code
 
-<<<<<<< HEAD
-	std::string vertexShaderPath = "shaders/vertex/ColorVertexShader.vert";
-	std::string fragmentShaderPath =
-		"shaders/fragment/ColorFragmentShader.frag";
-=======
 	std::string vertexShaderPath = "shaders/vertex/TextureTestShader.vert";
-	std::string fragmentShaderPath =
-		"shaders/fragment/TextureTestShader.frag";
->>>>>>> cc11259 (Scroll and moving objects)
+	std::string fragmentShaderPath = "shaders/fragment/TextureTestShader.frag";
 
 	window = glfwCreateWindow(1280, 720, "OpenGL Renderer", nullptr, nullptr);
 	if (!window) {
