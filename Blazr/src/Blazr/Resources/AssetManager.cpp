89ec27f7523e306e27885c9e902b4b6a7e06c4eb
Srkless <SrdjanGrujic01@outlook.com>
2024-11-04 17:09:16 +0100

    Asset Manager class

---
+++ Blazr/src/Blazr/Resources/AssetManager.cpp
@@ -0,0 +1,36 @@
+#include "Blazr/Core/Log.h"
+#include "Blazr/Renderer/ShaderLoader.h"
+#include "Blazr/Resources/AssetManager.h"
+
+bool Blazr::AssetManager::LoadTexture(const std::string &name,
+									  const std::string &texturePath,
+									  bool pixelArt) {
+	if (m_mapTextures.find(name) != m_mapTextures.end()) {
+		BLZR_CORE_ERROR("Texture already loaded: {0}", name);
+		return false;
+	}
+
+	// TODO: pixelArt
+	auto texture = std::make_shared<Texture2D>(texturePath);
+
+	if (!texture) {
+		BLZR_CORE_ERROR("Failed to load texture: {0}", texturePath);
+		return false;
+	}
+
+	m_mapTextures.emplace(name, std::move(texture));
+	return true;
+}
+const Blazr::Texture2D &
+Blazr::AssetManager::GetTexture(const std::string &name) {
+	auto textureIterator = m_mapTextures.find(name);
+	if (textureIterator == m_mapTextures.end()) {
+		BLZR_CORE_ERROR("Texture not found: {0}", name);
+		return;
+	}
+}
+
+bool Blazr::AssetManager::LoadShader(const std::string &name,
+									 const std::string &vertexPath,
+									 const std::string &fragmentPath) {}
+Blazr::Shader &Blazr::AssetManager::GetShader(const std::string &name) {}
