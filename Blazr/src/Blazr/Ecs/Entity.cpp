2f745e279d1c542c376f4a2e223424c837cfa45f
Mihajlo <103150495+ma115a@users.noreply.github.com>
2024-11-07 15:52:04 +0100

    Add files via upload

---
+++ Blazr/src/Blazr/Ecs/Entity.cpp
@@ -0,0 +1,24 @@
+#include "blzrpch.h"
+#include "Components/Identification.h"
+#include "Entity.h"
+
+Blazr::Entity::Entity(Registry &registry)
+	: Blazr::Entity(registry, "GameObject", "") {}
+
+Blazr::Entity::Entity(Registry &registry, const std::string &name,
+					  const std::string &group)
+	: m_Registry(registry), m_Name(name), m_Group(group),
+	  m_EntityHandler{registry.CreateEntity()} {
+	AddComponent<Identification>(
+		Identification{.name = name,
+					   .group = group,
+					   .id = static_cast<int32_t>(m_EntityHandler)});
+}
+Blazr::Entity::Entity(Registry &registry, const entt::entity &entity)
+	: m_Registry(registry), m_EntityHandler(entity), m_Name(""), m_Group("") {
+	if (m_Registry.GetRegistry().all_of<Identification>(m_EntityHandler)) {
+		auto id = GetComponent<Identification>();
+		m_Name = id.name;
+		m_Group = id.group;
+	}
+}
