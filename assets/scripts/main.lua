ce5508ed867c8d46e756723649d0881fa0aa50fa
ma115a <aaaaaaa2122w@gmail.com>
2024-11-14 17:39:16 +0100

    finalizing merge

--- assets/scripts/main.lua
+++ assets/scripts/main.lua
@@ -10,10 +10,13 @@ local transform2 = gEntity2:add_component(TransformComponent(vec2(100, 300), vec
 local sprite2 = gEntity2:add_component(SpriteComponent(200, 200, "masha", 0, 0, 0))
 local collider2 = gEntity2:add_component(BoxColliderComponent(200, 200, vec2(0, 0)))
 
-playerEntity = Entity("Player", "G1")
+playerEntity = Entity("Player", "G2")
 local transform = playerEntity:add_component(TransformComponent(500, 450, 1, 1, 0))
 local sprite = playerEntity:add_component(SpriteComponent(32.0, 32.0, "chammy", 0, 0, 0))
 local collider = playerEntity:add_component(BoxColliderComponent(200, 200, vec2(0, 0)))
+
+local soundPlayer = SoundPlayer.get_instance()
+
 
 local x = 0
 local max_x = 800
@@ -30,20 +33,16 @@ main = {
 
             if x >= max_x then
                 x = 0
-                soundPlayer:toggle_music_mute()
-                soundPlayer:play_effect("jump", 0, 1)
-                -- print(soundPlayer:get_channel_volume(1))
-                -- soundPlayer:set_channel_volume(1, soundPlayer:get_channel_volume(1) + 10)
-                -- soundPlayer:set_music_volume(soundPlayer:get_music_volume() + 10)
             end
 
             if BoxColliderSystem.is_colliding(gEntity2, playerEntity) then
                 print("Colliding")
             end
+            
+            if InputSystem.key_pressed(Key_Space) then
+                soundPlayer:play_effect("jump", 0, -1)
 
-            if BoxColliderSystem.is_colliding(gEntity2, playerEntity) then
-                print("Colliding")
-            end
+        end
 
             -- local s1 = gEntity2:remove_component(sprite2)
             -- print(s1.width)
