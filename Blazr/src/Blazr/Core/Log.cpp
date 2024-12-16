0522c45e5990cc4afd562d3709cd28ff43d69f2e
ma115a <aaaaaaa2122w@gmail.com>
2024-12-16 17:11:38 +0100

    redirected blazr log output to editor logs gui

--- Blazr/src/Blazr/Core/Log.cpp
+++ Blazr/src/Blazr/Core/Log.cpp
@@ -1,16 +1,35 @@
 #include "blzrpch.h"
 #include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks-inl.h"
+#include "spdlog/spdlog-inl.h"
 
 namespace Blazr {
 std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
+std::shared_ptr<ImGuiLogSink> Log::imGuiSink = std::make_shared<ImGuiLogSink>();
 void Log::init() {
-  spdlog::set_pattern("%^[%T] %n: %v%$");
-  s_CoreLogger = spdlog::stdout_color_mt("BLAZR");
-  s_CoreLogger->set_level(spdlog::level::trace);
+	// Create sinks
+	auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	consoleSink->set_pattern("%^[%T] %n: %v%$");
+	imGuiSink->set_pattern("[%T] %n: %v");
 
-  s_ClientLogger = spdlog::stdout_color_mt("APP");
-  s_ClientLogger->set_level(spdlog::level::trace);
+	// Drop existing loggers if they exist
+	spdlog::drop("BLAZR");
+	spdlog::drop("APP");
+
+	// Core logger
+	s_CoreLogger = std::make_shared<spdlog::logger>(
+		"BLAZR", spdlog::sinks_init_list{consoleSink, imGuiSink});
+	s_CoreLogger->set_level(spdlog::level::trace);
+	s_CoreLogger->flush_on(spdlog::level::trace);
+	spdlog::register_logger(s_CoreLogger);
+
+	// Client logger
+	s_ClientLogger = std::make_shared<spdlog::logger>(
+		"APP", spdlog::sinks_init_list{consoleSink, imGuiSink});
+	s_ClientLogger->set_level(spdlog::level::trace);
+	s_ClientLogger->flush_on(spdlog::level::trace);
+	spdlog::register_logger(s_ClientLogger);
 }
 } // namespace Blazr
--- Blazr/src/Blazr/Core/Log.h
+++ Blazr/src/Blazr/Core/Log.h
@@ -1,23 +1,27 @@
 #pragma once
+#include "Blazr/Core/ImGuiLogSink.h"
 #include "Core.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
 namespace Blazr {
 class BLZR_API Log {
-public:
-  static void init();
+  public:
+	static void init();
 
-  inline static std::shared_ptr<spdlog::logger> &getCoreLogger() {
-    return s_CoreLogger;
-  }
-  inline static std::shared_ptr<spdlog::logger> &getClientLogger() {
-    return s_ClientLogger;
-  }
+	static std::shared_ptr<spdlog::logger> &getCoreLogger() {
+		return s_CoreLogger;
+	}
+	static std::shared_ptr<spdlog::logger> &getClientLogger() {
+		return s_ClientLogger;
+	}
 
-private:
-  static std::shared_ptr<spdlog::logger> s_CoreLogger;
-  static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	static std::shared_ptr<ImGuiLogSink> &getImGuiSink() { return imGuiSink; }
+
+  private:
+	static std::shared_ptr<spdlog::logger> s_CoreLogger;
+	static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	static std::shared_ptr<ImGuiLogSink> imGuiSink;
 };
 } // namespace Blazr
 
@@ -27,15 +31,15 @@ private:
 #define BLZR_CORE_ERROR(...) ::Blazr::Log::getCoreLogger()->error(__VA_ARGS__)
 #define BLZR_CORE_FATAL(...) ::Blazr::Log::getCoreLogger()->fatal(__VA_ARGS__)
 #define BLZR_CORE_CRITICAL(...)                                                \
-  ::Blazr::Log::getCoreLogger()->critical(__VA_ARGS__)
+	::Blazr::Log::getCoreLogger()->critical(__VA_ARGS__)
 
 #define BLZR_CLIENT_TRACE(...)                                                 \
-  ::Blazr::Log::getClientLogger()->trace(__VA_ARGS__)
+	::Blazr::Log::getClientLogger()->trace(__VA_ARGS__)
 #define BLZR_CLIENT_INFO(...) ::Blazr::Log::getClientLogger()->info(__VA_ARGS__)
 #define BLZR_CLIENT_WARN(...) ::Blazr::Log::getClientLogger()->warn(__VA_ARGS__)
 #define BLZR_CLIENT_ERROR(...)                                                 \
-  ::Blazr::Log::getClientLogger()->error(__VA_ARGS__)
+	::Blazr::Log::getClientLogger()->error(__VA_ARGS__)
 #define BLZR_CLIENT_FATAL(...)                                                 \
-  ::Blazr::Log::getClientLogger()->fatal(__VA_ARGS__)
+	::Blazr::Log::getClientLogger()->fatal(__VA_ARGS__)
 #define BLZR_CLIENT_CRITICAL(...)                                              \
-  ::Blazr::Log::getClientLogger()->critical(__VA_ARGS__)
+	::Blazr::Log::getClientLogger()->critical(__VA_ARGS__)
